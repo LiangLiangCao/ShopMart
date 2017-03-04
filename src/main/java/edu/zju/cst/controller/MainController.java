0ad849ef10588f2d23b580473a77c5941e75c9fa
caoliang <526687570@qq.com>
2017-03-04 13:08:20 +0800

    主页

---
+++ src/main/java/edu/zju/cst/controller/MainController.java
@@ -0,0 +1,36 @@
+package edu.zju.cst.controller;
+
+import edu.zju.cst.bean.Product;
+import edu.zju.cst.bean.User;
+import edu.zju.cst.service.IProductService;
+import edu.zju.cst.service.IUserService;
+import edu.zju.cst.service.UserServiceImpl;
+import edu.zju.cst.service.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+
+/**
+ * Created by Liang on 19/02/2017.
+ */
+@Controller
+public class MainController extends BaseController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(ModelMap map) {
+
+        List<Product> productList = productService.getProducts(10,1);
+        map.put("latestProduct",productList);
+
+        return "welcome";
+    }
+
+}
