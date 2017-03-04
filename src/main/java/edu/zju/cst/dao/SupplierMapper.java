c868159f58a0d93cd38b2caaed034dff7587a4fb
liang <526687570@qq.com>
2017-03-04 22:34:24 +0800

    modify mapper

--- src/main/java/edu/zju/cst/dao/SupplierMapper.java
+++ src/main/java/edu/zju/cst/dao/SupplierMapper.java
@@ -1,9 +1,7 @@
 package edu.zju.cst.dao;
 
 import edu.zju.cst.bean.Supplier;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SupplierMapper {
     int deleteByPrimaryKey(Long supplierId);
 
@@ -16,6 +14,4 @@ public interface SupplierMapper {
     int updateByPrimaryKeySelective(Supplier record);
 
     int updateByPrimaryKey(Supplier record);
-    int deleteByUpdate(Long userId);
-
 }
